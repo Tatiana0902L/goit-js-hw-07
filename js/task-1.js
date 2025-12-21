f2b6047f983733f88398e47247a8e3124de3c604
Tetiana Lugova <tanjkee00@gmail.com>
2025-12-21 19:17:32 +0200

    work done

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const categoriesList = document.querySelector(`#categories`);
+const categoriesItems = document.querySelectorAll(`.item`);
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach(item => {
+    const title = item.querySelector(`h2`).textContent;
+    const elements = item.querySelectorAll(`li`).length;
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${elements}`);
+});
